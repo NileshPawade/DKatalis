74f766406163e6f52b37d59a1cc6972e056cef5b
neosoft <neosoft@Pune-Laptop136>
2020-03-07 17:01:02 +0530

    Created CheckOut Page with most of the web elements

---
+++ src/main/java/com/midtrans/demo/pages/CheckOutPage.java
@@ -0,0 +1,122 @@
+package com.midtrans.demo.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.midtrans.demo.BaseClass.TestBase;
+
+public class CheckOutPage extends TestBase {
+		
+		public CheckOutPage() {
+			super();
+			PageFactory.initElements(driver, this);
+		}
+		
+		@FindBy(css="input.text-right")
+		private WebElement pillowPrice;
+		
+		@FindBy(css="tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(2)")
+		private WebElement quantities;
+		
+		@FindBy(css="td.amount")
+		private WebElement totalAmount;
+		
+		@FindBy(xpath="(//input[@type='text'])[1]")
+		private WebElement customerName;
+		
+		@FindBy(xpath="//input[@type=\"email\"]")
+		private WebElement customerEmail;
+		
+		@FindBy(xpath="(//input)[4]")
+		private WebElement customerPhoneNumber;
+
+		@FindBy(xpath="(//input)[5]")
+		private WebElement customerCity;
+
+		@FindBy(css="textarea")
+		private WebElement customerAddress;
+		
+		@FindBy(xpath="(//input)[6]")
+		private WebElement customerPostalCode;
+		
+		@FindBy(css="div.cart-checkout")
+		private WebElement checkOutButton;
+		
+		@FindBy(css="div.cart-checkout-settings > img")
+		private WebElement checkOutSettingButton;
+		
+		@FindBy(css="div.cancel-btn")
+		private WebElement cancelButton;
+		
+		public WebElement getPillowPrice() {
+			return pillowPrice;
+		}
+
+		public String getPillowPriceStringValue() {
+			return pillowPrice.getAttribute("value");
+		}
+		
+		
+		public WebElement getQuantities() {
+			return quantities;
+		}
+
+
+		public WebElement getTotalAmount() {
+			return totalAmount;
+		}
+
+
+
+		public WebElement getCustomerName() {
+			return customerName;
+		}
+
+
+
+		public WebElement getCustomerEmail() {
+			return customerEmail;
+		}
+
+
+
+		public WebElement getCustomerPhoneNumber() {
+			return customerPhoneNumber;
+		}
+
+
+
+		public WebElement getCustomerCity() {
+			return customerCity;
+		}
+
+		
+		public WebElement getCustomerAddress() {
+			return customerAddress;
+		}
+
+
+		public WebElement getCustomerPostalCode() {
+			return customerPostalCode;
+		}
+
+
+
+		public WebElement getCheckOutButton() {
+			return checkOutButton;
+		}
+
+
+
+		public WebElement getCheckOutSettingButton() {
+			return checkOutSettingButton;
+		}
+
+
+
+		public WebElement getCancelButton() {
+			return cancelButton;
+		}
+
+}
--- src/test/java/com/midtrans/demo/testcases/ProductPurchaseTest.java
+++ src/test/java/com/midtrans/demo/testcases/ProductPurchaseTest.java
@@ -1,18 +1,66 @@
 package com.midtrans.demo.testcases;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.midtrans.demo.BaseClass.TestBase;
+import com.midtrans.demo.pages.CheckOutPage;
+import com.midtrans.demo.pages.ProductPage;
 
-public class ProductPurchaseSuccessFlowTest extends TestBase {
+public class ProductPurchaseTest extends TestBase {
 	
-	public ProductPurchaseSuccessFlowTest() {
+	public ProductPurchaseTest() {
 		super();
 	}
-	@Test
+	ProductPage productPage;
+	CheckOutPage checkOutPage;
+	
+	@BeforeTest
 	public void setup()
 	{
 		initialisation();
+		productPage=new ProductPage();
+		checkOutPage=new CheckOutPage();
+		
+	}
+	
+	@Test
+	public void ProductPurchaseSuccessFlowTest()
+	{
+		Assert.assertEquals(productPage.getBuyNowButton().isEnabled(), true);
+		productPage.getBuyNowButton().click();
+		
+		checkOutPage.getCustomerName().clear();
+		
+		checkOutPage.getCustomerName().sendKeys("Nilesh");
+		
+		checkOutPage.getCustomerEmail().clear();
+		
+		checkOutPage.getCustomerEmail().sendKeys("abc@gmail.com");
+		
+		checkOutPage.getCustomerPhoneNumber().clear();
+		
+		checkOutPage.getCustomerPhoneNumber().sendKeys("123434234234");
+		
+		checkOutPage.getCustomerCity().clear();
+		
+		checkOutPage.getCustomerCity().sendKeys("Pune");
+		
+		checkOutPage.getCustomerAddress().clear();
+		
+		checkOutPage.getCustomerAddress().sendKeys("Maharashtra,India");
+		
+		checkOutPage.getCustomerPostalCode().clear();
+		
+		checkOutPage.getCustomerPostalCode().sendKeys("3442232");
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.close();
 	}
 
 }
